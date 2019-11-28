7e8ddd37e2b3e08febf09cb66a05bb18526c7af7
Nam Anh Nguyen <namanh1@web.de>
2019-11-28 15:27:11 +0100

    HttpError-Handling, Article Model/Route (not done yet)

--- routes/users/users.js
+++ routes/users/users.js
@@ -2,36 +2,81 @@ const express = require("express");
 const router = express.Router();
 
 const User = require("../../models/User")
+const HttpError = require('../../middleware/httpError.js');
 
-// @route DELETE api/users/:id
-router.delete('/:id', (req, res, next) => {
-  User.findOneAndDelete({'_id': req.params.id})
-  .then(data => {
-    if (!data) res.status(404).json({"_id": "User not found"})
-    else res.json(data);
+router.route('/:id')
+  // @route GET api/users/:id
+  .get((req, res, next) => {
+    User.findById(req.params.id, '-password').then( user => {
+      if (!user){
+        let err = new HttpError("User Not Found", 404);
+        next(err);
+      }
+      else res.status(200).json(user);
+    })
+    .catch(err => {
+      err = new HttpError(err.message, 400);
+      next(err);
+    })
   })
-  .catch(next)
-})
+
+  // @route DELETE api/users/:id
+  .delete((req, res, next) => {
+    User.findOneAndDelete({'_id': req.params.id})
+    .then(data => {
+      if (!data) {
+        let err = new HttpError("User Not Found", 404);
+        next(err);
+      }
+      else res.status(204).end();
+    })
+    .catch(err => {
+      err = new HttpError(err.message, 400);
+      next(err);
+    })
+  })
+
+  // @route PATCH api/users/:id
+  .patch((req, res, next) => {
+
+  })
+
+  .all((req, res, next) => {
+    let err = new HttpError("Method Not Allowed", 405);
+    next(err);
+  });
 
 // @route GET api/users
-router.get('', (req, res, next) => {
-  User.find({}, 'name')
-  .then(data => res.json(data))
-  .catch(next)
-});
-
-// @route GET api/users/:id
-router.get('/:id', (req, res, next) => {
-  User.findById(req.params.id, '-password').then( user => {
-    if (!user) res.status(404).json({"_id": "User not found"})
-    else res.json(user);
+router.route('/')
+  .get((req, res, next) => {
+    User.find({}, 'name')
+    .then(data => res.status(200).json(data))
+    .catch(err => {
+      err = new HttpError(err.message, 400);
+      next(err);
+    })
   })
-});
 
-// @route PATCH api/users/:id
-router.patch('/:id', (req, res, next) => {
-  
-})
+  .post((req, res, next) => {
+    if(req.body.name && req.body.password){
+
+      User.create(req.body)
+      .then(data => res.status(200).json(data))
+      .catch(err => {
+        err = new HttpError(err.message, 400);
+        next(err);
+      })
+
+    } else {
+      err = new HttpError("The name or password field is empty", 400);
+      next(err);
+    }
+  })
+
+  .all((req, res, next) => {
+    let err = new HttpError("Method Not Allowed", 405);
+    next(err);
+  });
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,17 +3,15 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 // const routes = require('./routes/api');
-const usersAuthAPI = require("./routes/auth-api/users")
-const usersAPI = require("./routes/users/users")
+const userAuth = require("./routes/auth-api/userAuth")
+const users = require("./routes/users/users")
+const errorHandler = require("./middleware/errorHandler")
+const responseHeader = require("./middleware/responseHeader")
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(responseHeader);
 
 app.use(
   bodyParser.urlencoded({
@@ -43,8 +41,11 @@ require("./config/passport")(passport);
 
 // Routes
 // app.use('/api', routes); // Not safe users API
-app.use('/api/auth', usersAuthAPI)
-app.use('/api/users', usersAPI)
+app.use('/api/auth', userAuth);
+app.use('/api/users', users);
+
+// Error-Errorhandling
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
