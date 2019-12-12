c3e49c0c36ee69f0f353971a67e199cc158f7404
Michal Sienkiewicz <pizzaboi7@disroot.org>
2019-12-12 16:39:47 +0100

    added API for component requests

---
+++ client/src/utils/API.js
@@ -0,0 +1,32 @@
+import Axios from 'axios';
+
+export default {
+    getHot(callback){
+        Axios.get("http://localhost:5000/newsapi/top-headlines?country=de&pageSize=5")
+        .then(
+            (res) => {
+                callback({
+                    name: "Hot",
+                    type: "horizontal",
+                    articles: res.data.articles
+                })
+            }
+        ).catch(
+            err => console.log(err)
+        )
+    },
+    getLatest(callback){
+        Axios.get("http://localhost:5000/newsapi/latest?language=de")
+        .then(
+            (res) => {
+                callback({
+                    name: "Latest",
+                    type: "grid",
+                    articles: res.data.articles
+                })
+            }
+        ).catch(
+            err => console.log(err)
+        )
+    }
+} 
