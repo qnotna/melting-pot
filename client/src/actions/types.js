6ff33a823e3b74a2be97342111e72ec8746be2eb
Michal Sienkiewicz <pizzaboi7@disroot.org>
2019-12-16 20:02:44 +0100

    removed auth reducers, added simple redux store

--- client/src/actions/types.js
+++
@@ -1,7 +0,0 @@
-// An interaction (such as a button click or a form submission) 
-// in our React components will fire off an action and, in turn, 
-// dispatch an action to our store.
-
-export const GET_ERRORS = "GET_ERRORS";
-export const USER_LOADING = "USER_LOADING";
-export const SET_CURRENT_USER = "SET_CURRENT_USER";
--- client/src/store.js
+++ client/src/store.js
@@ -1,20 +1,8 @@
-// createStore() creates a Redux store that holds the complete state tree of your app. There should only be a single store in your app.
-// Our store sends application state to our React components, which will react accordingly to that state.
-
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import rootReducer from "./reducers"
+import { createStore } from "redux";
+import reducer from "./reducers"
 
 const initialState = {};
-const middleware = [thunk];
 
-const store = createStore(
-    rootReducer,
-    initialState,
-    compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-    )
-);
+const store = createStore(reducer, initialState);
 
 export default store;
