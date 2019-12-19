a1f8815e8a86714dce05c911434e498f155902ed
Jamine <s73092@beuth-hochschule.de>
2019-12-19 21:37:37 +0100

    sidebar animieren fertig

--- client/src/components/SideBar.js
+++ client/src/components/SideBar.js
@@ -8,10 +8,12 @@ class SideBar extends Component {
   render() {
     return(
       <div id="sidebar">
+        <div id='sidebar-scrollbar'>
         <ul className='sidebar-sections'>
           <SideBarSections sections={this.props.sections}/>
         </ul>
         <ProfileOverview user={this.props.user}/>
+        </div>
       </div>
     );
   }
