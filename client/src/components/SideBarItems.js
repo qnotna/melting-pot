b861b598f893c6dd9e15f4528f7b62dcb625a36d
Michal Sienkiewicz <pizzaboi7@disroot.org>
2019-12-19 23:34:58 +0100

    added sidebar redux actions

--- client/src/components/SideBarItems.js
+++ client/src/components/SideBarItems.js
@@ -1,16 +1,38 @@
 import React, {Component} from 'react';
+import store from '../store';
+
+import { setContentComponent } from "../actions"
 
 class SideBarItems extends Component {
 
   isDefaultChecked = (item) => ((item === 'Melting Hot') ? true : false);
   createItemKey = (item) => (`item-${item.toLowerCase().replace(' ', '-')}`)
 
+  setCurrentComponent(component){
+    store.dispatch( setContentComponent(component))
+  }
+
   render() {
     return this.props.items.map((item) => (
-      <li className='sidebar-item' key={this.createItemKey(item.title)}>
-        <input type='radio' name='sidebar-items' id={this.createItemKey(item.title)} defaultChecked={this.isDefaultChecked(item.title)}/>
-        <label className='sidebar-item-label' htmlFor={this.createItemKey(item.title)}>
-          <img src='http://www.clker.com/cliparts/a/1/C/x/k/E/pink-square-hi.png' alt='Item piktogram'/>
+      <li 
+        className='sidebar-item' 
+        key={this.createItemKey(item.title)}
+      >
+        <input 
+          type='radio' 
+          name='sidebar-items' 
+          id={this.createItemKey(item.title)} 
+          defaultChecked={this.isDefaultChecked(item.title)}
+        />
+        <label 
+          className='sidebar-item-label' 
+          htmlFor={this.createItemKey(item.title)} 
+          onClick={() => this.setCurrentComponent(item.component)}
+        >
+          <img 
+            src='http://www.clker.com/cliparts/a/1/C/x/k/E/pink-square-hi.png' 
+            alt='Item piktogram'
+          />
           <p className='sidebar-item-title' unread-amount={item.unreadAmount}>{item.title}</p>
         </label>
       </li>
