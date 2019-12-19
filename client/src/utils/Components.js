44b6a3bed1626ee6315f942a4bc48bfe069efdc3
Michal Sienkiewicz <pizzaboi7@disroot.org>
2019-12-19 23:24:42 +0100

    created enum for components

---
+++ client/src/utils/Components.js
@@ -0,0 +1,6 @@
+// Components Enum defining all parent components that 
+// could be renderd inside of the Home component
+export const Components = {
+    HOME: 'HOME',
+    SETTINGS: 'SETTINGS'
+}
