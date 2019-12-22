9a06edd0c12a55d2fb3a1454c6b9326aa44e3ac8
Michal Sienkiewicz <pizzaboi7@disroot.org>
2019-12-22 23:27:22 +0100

    added object containing newsapi options for building request

---
+++ client/src/data/options.js
@@ -0,0 +1,21 @@
+export const languages = {
+    de: 'german',
+    pl: 'polish',
+    ar: 'arabic',
+    en: 'english',
+    es: 'spanish',
+    fr: 'french',
+    he: 'hebrew',
+    it: 'italian',
+    nl: 'dutch',
+    no: 'norwegian',
+    pt: 'portuguese',
+    ru: 'russian',
+    zh: 'chinese'
+}
+
+export const sortOptions = {
+   relevancy: 'relevancy', // articles more closely related to q come first
+   popularity: 'popularity', // articles from popular sources and publishers come first
+   publishedAt: 'newest', // newest articles come first
+}
