6f873f5d3b754eb950fc94c85fe1f5cc661d507e
pizzaboi7 <pizzaboi7@disroot.org>
2020-01-25 23:16:15 +0100

    new newsApi key

--- routes/news-api/news-api.js
+++ routes/news-api/news-api.js
@@ -1,7 +1,8 @@
 const router = require ('express').Router();
 const NewsAPI  = require('newsapi');
 // const newsapi = new NewsAPI('1a1523a02e3d4a65a047b106d46acaaa');
-const newsapi = new NewsAPI('04cc2e205e294f27b2072a47d8ce57bd');
+// const newsapi = new NewsAPI('04cc2e205e294f27b2072a47d8ce57bd');
+const newsapi = new NewsAPI('e0da45e697234dbf8e89825c62e5dfbb');
 
 const HttpError = require('../../middleware/httpError');
 
